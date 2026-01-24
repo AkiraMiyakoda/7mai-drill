b47f29187209bdc430d16f694ea7ebcd1b6c06cf
Akira Miyakoda <akira.miyakoda@gmail.com>
2026-01-24 17:36:07 +0900

    最低限のPWA対応

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Copyright © 2026 Akira Miyakoda
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+self.addEventListener("fetch", async (_event) => {
+    return;
+});

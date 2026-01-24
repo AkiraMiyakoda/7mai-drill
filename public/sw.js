ea567b49b5743798cdb6920aafbcf1a3ec6f94ad
Akira Miyakoda <akira.miyakoda@gmail.com>
2026-01-24 19:49:49 +0900

    sw.js に console.log を追加

--- public/sw.js
+++ public/sw.js
@@ -7,6 +7,7 @@ const CACHE_NAME = "";
 const CACHE_FILES = [];
 
 self.addEventListener("install", async (e) => {
+    console.log("SW: install");
     e.waitUntil(async () => {
         const cache = await caches.open(CACHE_NAME);
         caches.addAll(CACHE_FILES);
@@ -14,8 +15,10 @@ self.addEventListener("install", async (e) => {
 });
 
 self.addEventListener("fetch", async (e) => {
+    console.log("SW: install");
     e.respondWith(async () => {
         const cacheResponse = await caches.match(e.request);
+        console.log("SW: cacheResponse:", cacheResponse);
         if (cacheResponse) {
             return cacheResponse;
         }
@@ -23,6 +26,8 @@ self.addEventListener("fetch", async (e) => {
         const fetchResponse = await fetch(e.request);
         const cache = await caches.open(CACHE_NAME);
         cache.put(e.request, fetchResponse.clone());
+        console.log("SW: fetchResponse:", fetchResponse);
+
         return fetchResponse;
     });
 });
